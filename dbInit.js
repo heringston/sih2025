f5087c8b7f37e8399eb971e8e53b061d37459014
heringston <thomas.a10242@gmail.com>
2025-10-04 22:08:07 +0530

    connected to cloud db

--- dbInit.js
+++ dbInit.js
@@ -1,23 +1,50 @@
-const client = require('./index');
+import { Client } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
 
 async function initTables() {
   try {
     await client.query(`
-      CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(50),
-        email VARCHAR(100),
-        password VARCHAR(100)
-      );
+      create  table if not exists users(
+sid varchar(100) primary key unique not null,
+password text not null);
+
     `);
 
     await client.query(`
-      CREATE TABLE IF NOT EXISTS transactions (
-        id SERIAL PRIMARY KEY,
-        amount NUMERIC,
-        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      );
+      create  table if not exists employer(
+company_id varchar(100) primary key unique not null,
+password text not null
+);
     `);
+
+    await client.query(`
+        create  table if not exists university(
+college_id varchar(100) primary key unique not null,
+password text not null
+);
+        `);
+    
+    const res = await client.query(`
+  SELECT table_name
+  FROM information_schema.tables
+  WHERE table_schema = 'public';
+`);
+console.log("📋 Tables:", res.rows);
 
     console.log("Tables created successfully!");
     process.exit(0);
--- index.js
+++ index.js
@@ -14,7 +14,10 @@ const db = new pg.Client({
   host: process.env.DB_HOST,
   database:process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 db.connect();
 
